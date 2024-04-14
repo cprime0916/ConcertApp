3ab255c761c565dfa75a6d3088ade5960d3bb065
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2024-04-15 00:00:38 +0800

    Update main.tsx
    Imported all css files from the src/css directory to the run program.

--- concertapp/src/main.tsx
+++ concertapp/src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.tsx'
-import './index.css'
+import './css/default.css'
+import './css/div.css'
+import './css/main.css'
+import './css/slider.css'
+import './css/theme.css'
 import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
