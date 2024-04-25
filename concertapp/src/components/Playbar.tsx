716794a2977ad684a25a85418483bcc2e4f20eef
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2024-04-25 21:45:00 +0800

    Add Playbar.tsx
    Added base code for the playbar component in the bottom div

---
+++ concertapp/src/components/Playbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+interface PlaybarProps {
+    AudioPath: string;
+}
+
+const Playbar: React.FC<PlaybarProps> = ({ AudioPath }: { AudioPath: string }) => {
+    return (
+        <>
+            { /* TODO: construct Playbar component */ }
+        </>
+    );
+};
+
+export default Playbar;
