92b322731b6cf3380f429fda9583f32083eeae6d
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2024-04-27 01:06:33 +0800

    Add error svg to the error component
    Bug with SVG position and size issues.

--- concertapp/src/components/Error.tsx
+++ concertapp/src/components/Error.tsx
@@ -1,7 +1,8 @@
-{ /* import ErrorSvg from "../assets/ErrorSvg"; */}
+import ErrorSvg from "../assets/ErrorSvg";
 const Error: React.FC = () => {
     return (
         <div className="container">
+            <ErrorSvg />
             <span>Sorry! The resource you are trying to search is unavailable, to troubleshoot this issue, you may use <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="anchor">this guide</a> to assist you.</span>
         </div>
     );
