de2824f9e9dbb91b93609b4067003d89b8521f14
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2024-04-26 11:20:38 +0800

    Update Playbar Component
    When the play button is pressed, there would be an alert to show the audio path. Please note that this will later be removed, and is just for testing purposes.

--- concertapp/src/components/Playbar.tsx
+++ concertapp/src/components/Playbar.tsx
@@ -4,13 +4,17 @@ interface PlaybarProps {
 }
 
 const Playbar: React.FC<PlaybarProps> = ({ AudioPath }: { AudioPath: string }) => {
+    const TestFunc = () => {
+        alert(AudioPath)
+    };
     return (
         <>
             <div className="2 play-button-out">
-                    <button className="1 play-button">⏵</button>
+                <button className="1 play-button" onClick={TestFunc}>⏵</button>
             </div>
             <div className="1 play-bar">
-                <button className="1 play-bar-button-left" style={{ left: '42%', width: '4%', zIndex: '107', borderRadius: '40% 0 0 40%' }}><img src="../assets/ShuffleIconDark.svg" style={{ width: '40px', height: '40px' }} className="img"></img></button>
+                <button className="1 play-bar-button-left" style={{ left: '42%', width: '4%', zIndex: '107', borderRadius: '40% 0 0 40%' }}>🔀</button>
+                { /* <button className="1 play-bar-button-left" style={{ left: '42%', width: '4%', zIndex: '107', borderRadius: '40% 0 0 40%' }}><img src="../assets/ShuffleIconDark.svg" style={{ width: '40px', height: '40px' }} className="img"></img></button> */}
                 <button className="1 play-bar-button-left" style={{ left: '45%', width: '3%', zIndex: '108', borderRadius: '50% 0 0 50%' }}>⏮</button>
                 <button className="1 play-bar-button-right" style={{ left: '52%', width: '3%', zIndex: '108', borderRadius: '0 50% 50% 0' }}>⏭</button>
                 <button className="1 play-bar-button-right" style={{ left: '54%', width: '4%', zIndex: '107', borderRadius: '0 40% 40% 0' }}>⏭</button>
