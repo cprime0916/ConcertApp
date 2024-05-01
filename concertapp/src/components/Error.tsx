70458ca0a96d9515c4f1e09df94d85cd6f5400e0
Raymond Yeung <nothinghere19374@gmail.com>
2024-05-01 21:51:14 +0800

    Fixed Error SVG
    Update dependencies, the text is shown properly now.

--- concertapp/src/components/Error.tsx
+++ concertapp/src/components/Error.tsx
@@ -1,9 +1,9 @@
 import ErrorSvg from "../assets/Error.svg";
 const Error: React.FC = () => {
     return (
-        <div className="container">
+        <div className="main container">
             <ErrorSvg />
-            <span>Sorry! The resource you are trying to search is unavailable, to troubleshoot this issue, you may use <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="anchor">this guide</a> to assist you.</span>
+            <h6 className="error-text">Sorry! The resource you are trying to search is unavailable, to troubleshoot this issue, you may use <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="anchor">this guide</a> to assist you.</h6>
         </div>
     );
 };
