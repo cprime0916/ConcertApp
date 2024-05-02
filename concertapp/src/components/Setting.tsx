9042be00dfa95d855bf2df2dafa485f5d1073a0d
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2024-05-02 22:24:43 +0800

    Add Setting.tsx
    Add setting TSX component init code.

---
+++ concertapp/src/components/Setting.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Setting: React.FC = () => {
+    return (
+        <>
+            { /* TODO: make settings component */ }
+        </>
+    );
+};
+export default Setting;
