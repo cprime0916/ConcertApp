e42d2d7eff9fe3b87c3c645450a081238a281e9b
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2024-05-05 18:56:55 +0800

    Add Setting SVG & Update Setting.tsx
    Made basic structure code for the setting component.

--- concertapp/src/components/Setting.tsx
+++ concertapp/src/components/Setting.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import SettingSVG from "../assets/Setting.svg";
+interface TribarProps {
+    options: string[];
+    DivName: string;
+}
+const Tribar: React.FC<TribarProps> = ({ options }: { options: string[] }) => {
+    const ToggleFunc = () => {
+        {/* TODO: Construct toggle function */ }
+    };
+    return (
+        <>
+            {/* TODO: construct tribar component (base component of Setting */}
+        </>
+    );
+};
 const Setting: React.FC = () => {
     return (
         <>
+            ( localStorage.getItem("theme") === 'dark' ? <SettingSVG className="dark-inverse" /> : <SettingSVG className="light-inverse" />)
             { /* TODO: make settings component */ }
         </>
     );
