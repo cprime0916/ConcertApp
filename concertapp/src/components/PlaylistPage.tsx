560bbfcf4193d51c1ac26081c11bade28a84ab68
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2024-05-18 21:44:25 +0800

    Update PlaylistPage.tsx & TSLogo SVG - the component will redirect to Error page if a playlist does not exist - Indent the SVG file properly

--- concertapp/src/components/PlaylistPage.tsx
+++ concertapp/src/components/PlaylistPage.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import TSLogo from "../assets/TSLogo.svg";
+import Error from "./Error";
+const namesArray = ["Foo", "Bar"];
+const playlistNameFmt = (s: string) => {
+    s = s[0].toUpperCase() + s.slice(1);
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] === '_' && i + 1 < s.length) {
+            s = s.slice(0, i) + ' ' + s[i + 1].toUpperCase() + s.slice(i + 2);
+        }
+    }
+    return s;
+};
 const PlaylistPage: React.FC = () => {
     const { playlistName } = useParams();
+    const refmtName = playlistNameFmt(playlistName!);
+    if (!namesArray.includes(refmtName)) {
+        return <Error />;
+    }
     return (
-        <div>
+        <div className="1">
             <TSLogo />
-            <h1>{playlistName}</h1>
+            <h1>{refmtName}</h1>
         </div>
     )
 };
